9abb3954b3366d8e51c32210f057b4c3145445ae
louiscklaw <louiscklaw@gmail.com>
2020-07-15 17:26:44 +0800

    upadte for html reporter,

--- nightwatch/scripts/new_nightwatch_helloworld.sh
+++ nightwatch/scripts/new_nightwatch_helloworld.sh
@@ -7,7 +7,7 @@ set -ex
 
 TEST=`echo $PWD|rev |cut -d'/' -f1 |rev`
 
-# git checkout -b test/$TEST
+git checkout -b test/$TEST
 
 
 rsync -avz --exclude 'node_modules' --exclude 'public' ../nightwatch-helloworld/ .
