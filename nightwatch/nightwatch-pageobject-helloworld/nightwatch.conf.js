08b2cd31e972751b06592ffd8a0ddb9bb88c7de0
louiscklaw <louiscklaw@gmail.com>
2020-07-16 00:41:07 +0800

    commit after test,

--- nightwatch/nightwatch-pageobject-helloworld/nightwatch.conf.js
+++ nightwatch/nightwatch-pageobject-helloworld/nightwatch.conf.js
@@ -44,7 +44,7 @@ module.exports = {
             //'--no-sandbox',
             '--ignore-certificate-errors',
             '--allow-insecure-localhost',
-            // '--headless'
+            '--headless'
           ]
         }
       },
