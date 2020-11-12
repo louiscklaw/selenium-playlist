4126cb4640b2251b9438f69a68d73c703deedc11
louiscklaw <louiscklaw@gmail.com>
2020-11-12 13:58:37 +0800

    update working commit.

--- chrome-selenium-hub-tryout/scripts/up_docker_selenium.sh
+++ chrome-selenium-hub-tryout/scripts/up_docker_selenium.sh
@@ -9,5 +9,5 @@ PATH=$PATH:$REPO_HOME/chrome-helloworld-tryout
 
 cd $REPO_HOME/_ref/docker-selenium
   docker-compose -f docker-compose-v3.yml pull
-  docker-compose -f docker-compose-v3.yml up
+  docker-compose -f docker-compose-v3.yml up -d
 cd $REPO_HOME
