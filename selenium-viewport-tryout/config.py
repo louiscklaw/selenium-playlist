eebfe7517f9a12f5a64a766472b4199e0bd9dfa4
louiscklaw <louiscklaw@gmail.com>
2020-11-13 20:38:55 +0800

    update and test,

--- selenium-viewport-tryout/config.py
+++
@@ -1,5 +0,0 @@
-import os,sys
-from pprint import pprint
-
-PROJ_HOME=os.path.dirname(__file__)
-SCREENCAPTURE_PATH=PROJ_HOME+'/screenshot'
--- selenium-viewport-tryout/run.sh
+++ selenium-viewport-tryout/run.sh
@@ -2,8 +2,20 @@
 
 set -ex
 
+TMP_ORPHAN_BRANCH=$(mktemp -d)
+
+function shutdown {
+    # ./scripts/down_docker_selenium.sh
+    # rm -rf $TMP_ORPHAN_BRANCH
+    echo "Shutdown complete"
+}
+
+trap shutdown SIGTERM SIGINT EXIT
+
 REPO_HOME=..
 PATH=$PATH:$REPO_HOME/drivers/chrome/85
 
-pipenv sync
-pipenv run python3 ./main.py
+pushd src
+  pipenv sync
+  pipenv run python3 ./main.py
+popd
