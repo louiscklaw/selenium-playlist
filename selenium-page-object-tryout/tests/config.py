09cb2003834156702df91a9b7645add1eb16a460
louiscklaw <louiscklaw@gmail.com>
2020-11-13 19:07:04 +0800

    fix relative path and test,

--- selenium-page-object-tryout/tests/config.py
+++ selenium-page-object-tryout/tests/config.py
@@ -6,4 +6,6 @@ ENV_CI_TEST=os.getenv("CI_TEST")
 SELENIUM_HUB_HOST='localhost'
 
 SRC_DIR=os.path.dirname(__file__)
-SCREENSHOT_DIR=SRC_DIR+'/screenshot'
+TESTS_DIR=os.path.dirname(__file__)
+PROJ_HOME=os.path.abspath(TESTS_DIR+'/..')
+SCREENSHOT_DIR=os.path.abspath(PROJ_HOME+'/screenshot')
