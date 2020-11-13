52863cc2873800a36ba2d771a11d00e96804aed7
louiscklaw <louiscklaw@gmail.com>
2020-11-13 20:09:45 +0800

    update and test,

---
+++ selenium-viewport-tryout/config.py
@@ -0,0 +1,5 @@
+import os,sys
+from pprint import pprint
+
+PROJ_HOME=os.path.dirname(__file__)
+SCREENCAPTURE_PATH=PROJ_HOME+'/screenshot'
--- selenium-viewport-tryout/run.sh
+++ selenium-viewport-tryout/run.sh
@@ -2,23 +2,8 @@
 
 set -ex
 
-# export CI=1
-# export CI_TEST=1
-# export GITHUB_REPOSITORY=$(GET_SHORT_REPOSITORY)
+REPO_HOME=..
+PATH=$PATH:$REPO_HOME/drivers/chrome/85
 
-function shutdown {
-    ./scripts/down_docker_selenium.sh
-    echo "Shutdown complete"
-}
-
-trap shutdown SIGTERM SIGINT EXIT
-
-# export REPO_HOME=..
-# PATH=$PATH:$REPO_HOME/drivers/chrome/85
-
-./scripts/up_docker_selenium.sh &
-
-echo 'sleep a while to let docker steady'
-sleep 30
-
-./test.sh
+pipenv sync
+pipenv run python3 ./main.py
