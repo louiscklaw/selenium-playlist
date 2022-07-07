0331bbae4afbb104e6345565387e8c65b410f47f
louiscklaw <louiscklaw@gmail.com>
2022-07-07 19:14:31 +0800

    update selenium-helloworld,

---
+++ nightwatch-selenium-tryout/tests/helloworld.js
@@ -0,0 +1,15 @@
+describe("example.com Demo", function () {
+  before((browser) => browser.url("https://www.example.com/"));
+
+  test("Demo test ecosia.org", function (browser) {
+    browser
+      .waitForElementVisible("body")
+      .assert.titleContains("Example")
+      .assert.visible("h1")
+      .saveScreenshot("example.png");
+    // .waitForElementVisible('.small-footer-link-item')
+    // .assert.containsText('.mainline-results', 'Nightwatch.js')
+  });
+
+  after((browser) => browser.end());
+});
